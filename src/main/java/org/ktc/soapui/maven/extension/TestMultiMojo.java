5772bd22b670e5f20a99afb57f3474bb18a67f73
redfish4ktc <red4ktc-github987225@yahoo.fr>
2014-01-12 00:59:38 +0100

    Use includes/excludes to found soapui project #88
    Previously, we needed to list all projects that we wanted to run. Now, we
    can scan directory and configure which project have to be
    included/excluded from the soapui test run.
    Several scans can be configured (usefull when several directories need to
    be checked)

--- src/main/java/org/ktc/soapui/maven/extension/TestMultiMojo.java
+++ src/main/java/org/ktc/soapui/maven/extension/TestMultiMojo.java
@@ -19,19 +19,56 @@ package org.ktc.soapui.maven.extension;
 
 import static org.ktc.soapui.maven.extension.impl.runner.SoapUITestCaseRunnerWrapper.newSoapUITestCaseRunnerWrapper;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.DirectoryScanner;
 
 public class TestMultiMojo extends TestMojo {
 
-    private List<String> projectFiles;
+    private List<ProjectFilesScan> projectFiles;
 
     @Override
     protected void performRunnerExecute() throws MojoExecutionException, MojoFailureException {
-        for (String currentProjectFile : projectFiles) {
-            configureAndRun(newSoapUITestCaseRunnerWrapper(runnerType), currentProjectFile);
+        List<File> resolvedProjectFiles = resolveProjectFiles();
+        for (File currentProjectFile : resolvedProjectFiles) {
+            configureAndRun(newSoapUITestCaseRunnerWrapper(runnerType), currentProjectFile.getAbsolutePath());
         }
+    }
+
+    private List<File> resolveProjectFiles() {
+        List<File> resolved = new ArrayList<File>();
+        for (ProjectFilesScan scan : projectFiles) {
+            DirectoryScanner scanner = new DirectoryScanner();
+            File baseDirectory = scan.baseDirectory;
+            // TODO add FollowSymlink?
+            scanner.setBasedir(baseDirectory);
+            // null check to keep includes default ("**")
+            if (scan.includes != null) {
+                scanner.setIncludes(toArray(scan.includes));
+            }
+            scanner.setExcludes(toArray(scan.excludes));
+            scanner.scan();
+
+            String[] includedFiles = scanner.getIncludedFiles();
+            for (String includedFile : includedFiles) {
+                resolved.add(new File(baseDirectory, includedFile));
+            }
+        }
+        return resolved;
+    }
+
+    private static String[] toArray(Set<String> set) {
+        return set == null ? null : set.toArray(new String[set.size()]);
+    }
+
+    public static class ProjectFilesScan {
+        public File baseDirectory;
+        public Set<String> includes;
+        public Set<String> excludes;
     }
 
 }
