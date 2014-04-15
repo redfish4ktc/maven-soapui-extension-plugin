ef04594ec1d6e543ee462e98e62f035c1893adfc
redfish4ktc <red4ktc-github987225@yahoo.fr>
2014-04-15 22:42:20 +0200

    Format code
    Mainly remove extra spaces

--- src/main/java/org/ktc/soapui/maven/extension/impl/ErrorHandler.java
+++ src/main/java/org/ktc/soapui/maven/extension/impl/ErrorHandler.java
@@ -27,11 +27,11 @@ import org.apache.commons.lang3.reflect.FieldUtils;
 public class ErrorHandler {
 
     public static boolean hasFailures(SoapUITestCaseRunner runner) {
-        List < TestCase > failedTests = getFailedTests(runner);
+        List<TestCase> failedTests = getFailedTests(runner);
         if (failedTests.size() > 0) {
             return true;
         }
-        List < TestAssertion > failedAssertions = getFailedAssertions(runner);
+        List<TestAssertion> failedAssertions = getFailedAssertions(runner);
         if (failedAssertions.size() > 0) {
             return true;
         }
@@ -39,25 +39,24 @@ public class ErrorHandler {
         return false;
     }
 
-    private static List < TestCase > getFailedTests(SoapUITestCaseRunner runner) {
+    private static List<TestCase> getFailedTests(SoapUITestCaseRunner runner) {
         String fieldName = "failedTests";
         Field field = FieldUtils.getField(SoapUITestCaseRunner.class, fieldName, true);
         try {
             @SuppressWarnings("unchecked")
-            List < TestCase > failedTests = (List < TestCase >) FieldUtils.readField(field, runner, true);
+            List<TestCase> failedTests = (List<TestCase>) FieldUtils.readField(field, runner, true);
             return failedTests;
         } catch (IllegalAccessException e) {
             throw new RuntimeException("Unable to read field " + fieldName, e);
         }
     }
 
-    private static List < TestAssertion > getFailedAssertions(SoapUITestCaseRunner runner) {
+    private static List<TestAssertion> getFailedAssertions(SoapUITestCaseRunner runner) {
         String fieldName = "assertions";
         Field field = FieldUtils.getField(SoapUITestCaseRunner.class, fieldName, true);
         try {
             @SuppressWarnings("unchecked")
-            List < TestAssertion > failedAssertionss = (List < TestAssertion >) FieldUtils.readField(field, runner,
-                    true);
+            List<TestAssertion> failedAssertionss = (List<TestAssertion>) FieldUtils.readField(field, runner, true);
             return failedAssertionss;
         } catch (IllegalAccessException e) {
             throw new RuntimeException("Unable to read field " + fieldName, e);
