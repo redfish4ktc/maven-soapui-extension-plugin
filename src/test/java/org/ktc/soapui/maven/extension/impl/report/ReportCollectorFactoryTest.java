280f5aab8e7529ec1df6b646fc2448b7e9603466
redfish4ktc <red4ktc-github987225@yahoo.fr>
2014-04-24 16:53:06 +0200

    Use assertj in java tests #91
    Remove use of fest-assert 2.x in favor of assertj (more active, community
    driven, more assertions)

--- src/test/java/org/ktc/soapui/maven/extension/impl/report/ReportCollectorFactoryTest.java
+++ src/test/java/org/ktc/soapui/maven/extension/impl/report/ReportCollectorFactoryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Thomas Bouffard (redfish4ktc)
+ * Copyright 2013-2014 Thomas Bouffard (redfish4ktc)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 
 package org.ktc.soapui.maven.extension.impl.report;
 
-import static org.fest.assertions.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.ktc.soapui.maven.extension.impl.report.ReportCollectorFactory.newReportCollector;
 
 import org.junit.Test;
